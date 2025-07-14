963889914d628355ad0451b2fbec0f22cccb39d6
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-07-14 15:50:04 +0200

    LayoutHeader DONE

--- Frontend/src/components/LayoutHeader.jsx
+++ Frontend/src/components/LayoutHeader.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { NavLink } from "react-router";
+import "./LayoutHeader.scss"
+
+/** 
+ * Add a if condition to change div 'profilConnectionHeader'
+ * to change the content if the user is connected
+ *  <div className='profilConnectionHeader'>
+ *      <p>$UserName</p>
+ *      <NavLink to={"/"}>
+            <i class="fa-solid fa-right-from-bracket"></i>
+            <div className='signIn'>Sign Out</div>
+        </NavLink>
+ * </div>
+**/
 
 function LayoutHeader() {
   return (
-    <div>LayoutHeader</div>
+    <div className='layoutHeader'>
+        <NavLink to={"/"}>
+            <img className="logo" src=".\src\assets\argentBankLogo.png" alt="logo"/>  
+        </NavLink>
+        <div className='signInOut'>
+            <i class="fa-solid fa-circle-user fa-l"></i>
+            <div className='profilConnectionHeader'>
+                <NavLink to={"/SignIn"}>
+                    <div className='signIn'>Sign In</div>
+                </NavLink>
+            </div>
+      </div>
+    </div>
   )
 }
 
