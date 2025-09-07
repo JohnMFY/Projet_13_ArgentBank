6347a528289d099a2ec332b11eb0a150139beb96
Jonathan Mallefroy <jonathan.mallefroy@gmail.com>
2025-09-07 19:16:19 +0200

    Cleaning

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, /*useParams*/} from "react-router-dom"; //use of useParams for userPage/:id
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import store from './api/store.js'
 import { Provider } from 'react-redux'
 
